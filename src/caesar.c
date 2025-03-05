ccac45397cf58b664c045571def97132275667c1
0x0th30 <wrk.0x0th30@proton.me>
2025-03-05 09:39:14 -0300

    feat: pivotes from args input to interactive CLI

--- src/caesar.c
+++ src/caesar.c
@@ -4,48 +4,48 @@
 #include <string.h>
 
 #define ALPHABET_SIZE 26
-#define DEFAULT_ROTATION 1
 #define ENCODE 1
 #define DECODE -1
 
-void usage() {
-  printf("Usage: caesar [mode] [string] \n"
-         "-d         Decodes provided string\n"
-         "-e         Encodes provided string\n");
-}
-
 char cipher_char(char character, int rotation, int mode) {
   if (isupper(character))
-    return 'A' + (character - 'A' + (rotation * mode) +
-                  ALPHABET_SIZE % ALPHABET_SIZE);
+    return 'A' + (character - 'A' + (rotation * mode) + ALPHABET_SIZE) %
+                     ALPHABET_SIZE;
 
   if (islower(character))
-    return 'a' + (character - 'a' + (rotation * mode) +
-                  ALPHABET_SIZE % ALPHABET_SIZE);
+    return 'a' + (character - 'a' + (rotation * mode) + ALPHABET_SIZE) %
+                     ALPHABET_SIZE;
 
   return character;
 }
 
-int get_mode(char arg[]) {
-  if (arg[0] == '-') {
-    if (arg[1] == 'e')
-      return ENCODE;
-    if (arg[1] == 'd')
-      return DECODE;
-  }
+int get_mode(char char_mode) {
+  if (char_mode == 'E')
+    return ENCODE;
+  if (char_mode == 'D')
+    return DECODE;
 
-  usage();
+  printf("Invalid input, mode should be (D)ecode or (E)ncode.");
   exit(EXIT_FAILURE);
 }
 
-int main(int argc, char **argv) {
-  if (argc != 3) {
-    usage();
-    exit(EXIT_FAILURE);
-  }
+int main() {
+  char mode_input;
+  printf("Type the desired mode (D)ecode/(E)ncode: ");
+  scanf("%c", &mode_input);
+  getchar();
 
-  int mode = get_mode(argv[1]);
-  char *input = argv[2];
+  int mode = get_mode(mode_input);
+
+  int rotation;
+  printf("Type the rotation key: ");
+  scanf("%d", &rotation);
+  getchar();
+
+  char input[500];
+  printf("Type the entry string: ");
+  fgets(input, sizeof(input), stdin);
+
   size_t input_len = strlen(input);
 
   char *result = malloc(input_len + 1);
@@ -55,12 +55,11 @@ int main(int argc, char **argv) {
   }
 
   for (int i = 0; i < input_len; i++) {
-    result[i] = cipher_char(input[i], DEFAULT_ROTATION, mode);
+    result[i] = cipher_char(input[i], rotation, mode);
   }
   result[input_len] = '\0';
 
-  printf("%s version of \"%s\": %s\n", mode == 1 ? "Encoded" : "Decoded", input,
-         result);
+  printf("\n%s string: %s\n", mode == 1 ? "Encoded" : "Decoded", result);
 
   free(result);
 
